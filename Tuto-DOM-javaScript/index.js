b1fedab4859bcab8864b971a3118ba186e00cb57
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-08-02 22:42:27 +0000

    atelier meet

---
+++ Tuto-DOM-javaScript/index.js
@@ -0,0 +1,99 @@
+// SELECTEUR
+// document.querySelector('h4').style.background="red";
+
+// Click event
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+if(questionContainer){
+
+questionContainer.addEventListener("click", () => {
+    questionContainer.classList.toggle("question-clicked");
+
+});
+
+}
+
+btn1.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.background = "green"
+});
+btn2.addEventListener("click", () => {
+    response.classList.add("show-response");
+    response.style.background = "red";
+});
+
+//------------------Mouse Events------------------------
+
+const mousemove = document.querySelector(".mousemove");
+window.addEventListener("mousemove", (e) => {
+    mousemove.style.left = e.pageX + "px";
+    mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+    mousemove.style.transform = "scale(2) Translate(-25%, -25%)";
+});
+
+window.addEventListener("mouseup", () => {
+    mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+    mousemove.style.border = "2px solid teal";
+});
+
+if(questionContainer){
+questionContainer.addEventListener('mouseenter', () => {
+    questionContainer.style.background = "rgba(0,0,0,0.6)";
+});
+}
+
+if (questionContainer){   
+questionContainer.addEventListener("mouseout", () => {
+    questionContainer.style.background = "pink";
+});
+}
+
+response.addEventListener("mouseover", () => {
+    response.style.transform = "rotate(2deg)";
+});
+
+
+//============================keypress=======================
+
+const keypressContainer = document.querySelector(".keypress")
+const key = document.getElementById("key");
+
+document.addEventListener("keypress", (e) => {
+    key.textContent = e.key;
+
+    if (e.key === "j" ) {
+      keypressContainer.style.background = "pink";  
+    }else if (e.key === "h"){
+        keypressContainer.style.background = "teal";
+    }else{
+        keypressContainer.style.background = "red";
+    }
+});
+
+//============================Scroll  Event==========================================
+
+const nav= document.querySelector("nav");
+ 
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+
+    if(window.scrollY > 120) {
+        nav.style.top = 0;
+    }else{
+        nav.style.top = "-50px";
+    }
+});
+
+//=================================form Events==========================================
+
+const inputName = document.querySelector('input[type="text"]');
+let pseudo = "";
+inputName.addEventListener("input", (e) => {
+    pseudo = e.target.value;
+})
--- atelier meet/main.js
+++ atelier meet/main.js
@@ -1,3 +1,26 @@
-const list = document.getElementById(list);
-const 
+const main = document.getElementById('class');
+function afficher(codes, nom) {
+    const code = document.createElement(codes)
+    const textCode =  document.createTextNode(nom)
+    code.append(textCode);
+    main.append(code)
+}
+
+afficher('p', "On fait Un meet");
+afficher('div', "On fait Un meet");
+afficher('section', "On fait Un meet");
+afficher('li', "On fait Un meet");
+
+// const main = document.getElementById('class');
+
+// function afficher(balises, nom) {
+//    const balise = document.createElement(balises)
+
+//    const textBalise = document.createTextNode(nom);
+
+//    balise.append(textBalise);
+//     main.append(balise)
+// }
+
+// afficher('h1',"ok");
 
