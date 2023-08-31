9b73403e8d2773fbc8bf2f581d4089eb6307d8f2
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-08-31 18:36:28 +0000

    hours

--- Atelier-5-javaScript/Video a suivre/projet04/projet04.js
+++ Atelier-5-javaScript/Video a suivre/projet04/projet04.js
@@ -13,6 +13,12 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+// Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+
 // Stop Watch function-----------------------
 
 function stopWatch() {
@@ -28,12 +34,67 @@ function stopWatch() {
         }
     }
 
+    if(seconds < 10){
+        leadingSeconds = "0" + seconds.toString();
+
+    } else{
+        leadingSeconds = seconds
+    }
+
+    if(minutes < 10) {
+        leadingMinutes = "0" + minutes.toString();
+
+    } else {
+        leadingMinutes = minutes;
+
+    } 
+
+    if(hours < 10) {
+
+        leadingHours = "0" + hours.toString();
+
+    } else {
+
+        leadingHours = hours;
+    }
+
+ 
+
     let displayTimer = document.getElementById('timer').innerText =
-     hours + ":" + minutes + ":" + seconds;
+     leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
 }
 
-window.setInterval(stopWatch, 1000)
+// window.setInterval(stopWatch, 1000)
 
+
+startStopBtn.addEventListener('click', function() {
+   
+    if(timerStatus === "stopped") {
+        timerInterval = window.setInterval(stopWatch, 1000);
+
+        document.getElementById('startStopBtn').innerHTML = `
+        <i class="fa-solid fa-pause" id="pause"></i>`;
+        timerStatus = "started";
+
+    } else {
+        window.clearInterval(timerInterval);
+        document.getElementById('starStopBtn').innerHTML = `
+        <i class="fa-solid fa-play" id="play"></i>`;
+
+        timerStatus = "stopped";
+    }
+})
+
+
+resetBtn.addEventListener('click', function() {
+
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    document.getElementById('timer').innerHTML = "00:00:00"
+})
 //------------ document/getElementById-----------------------------
 
 
