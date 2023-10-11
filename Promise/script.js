33faec75c02c71a9a82fe20c449050eed80f6690
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-10-11 23:18:42 +0000

    changement

--- Promise/script.js
+++ Promise/script.js
@@ -1,53 +1,86 @@
-const avoiUnvelo = new Promise((resolve, reject) => {
-
-    let moyenne = 15;
-
-    if(moyenne < 10){
-        return('')
-    }
-})
-/*
-Division de a/b avec b!=0
-*
- */
 
 
-//  const division =   new Promise((Resolve, Reject) => {
-//      let a = 8;
+ //const division = new Promise((resolve, reject) => {
+//      let a = 10;
 //      let b = 2;
-//      let somme = a / b
-//      if(b === 0){
-//          return Reject('b est different a zero')
-//      } 
-    
-//      Resolve(somme)
+//      let somme = a / b;
+//      if (b == 0)  {
+//          return reject("b doit different a zero")
+//      }
 
-//  })
+//      resolve(`voivi le resultat du division : ${somme}`)
+ 
 //  division
-//  .then((res) =>{
+//  .then((res) => {
 //      console.log(res);
-//  }).catch((err) => {
-//  console.log(err);
+//  })
+//  .catch((err) => {
+//      console.log(err);
 //  })
 
 
-// promise sans fonction
+// DIVISION
+ const division = (a, b ) => {
+     return new Promise ((resole, reject) => {
 
+         if(b !== 0){
+             resole(a +' / ' + b + ' = ' + a / b)
+         }
+
+         reject('b doit different a zero')
+     })
+ }
+ division(20, 2)
+ .then(res => {
+     console.log(res)
+ })
+ .catch(err => {
+     console.log(err);
+ })
+
+ MULTIPLICATION
+let a = Number(prompt('enter a'))
+  let b = Number(prompt('enter b'))
+ const multiplication = (a , b ) => {
+     return new Promise ((valid, reject) => {
+         let egal = a * b
+         if (b == 0 ){
+
+            
+            
+             return reject('b doit difference a zero')
+
+         }
+
+         valid(`voici le resultat : ${egal}`)
+     })
+ }
+ multiplication(a, b)
+ .then(res => {
+     console.log(res);
+ })
+ .catch(err => {
+     console.log(err);
+ })
+
+ SOUSTRACTION
   let a = Number(prompt('enter a'))
   let b = Number(prompt('enter b'))
+  const soustraction = (a , b ) => {
+      return new Promise ((valid, reject) => {
+          let differ = a - b
+          if (a < b  || a <= 0){
 
-  const division = (a, b) => {
-      return new Promise ((resolve, reject) => {
-          if(b !== 0) {
-              return resolve(a + ' / ' + b + ' = ' + a / b)
+            
+            
+              return reject('a doit supperieur a b')
+
           }
 
-          reject('le b doit diffrent a Zero' )
+          valid(`voici le resultat : ${differ}`)
       })
-  };
-
-  division(a, b)
-  .then(res => {
+  }
+  soustraction(a, b).then(res => {
       console.log(res);
   })
   .catch(err => {
@@ -57,121 +90,55 @@ Division de a/b avec b!=0
 
 
 
-
-// const division = new Promise((resolve, reject) => {
-//     let a = 10;
-//     let b = 2;
-//     let somme = a / b;
-//     if (b == 0)  {
-//         return reject("b doit different a zero")
-//     }
-
-//     resolve(`voivi le resultat du division : ${somme}`)
-// })
-// division
-// .then((res) => {
-//     console.log(res);
-// })
-// .catch((err) => {
-//     console.log(err);
-// })
-
-
-// DIVISION
-// const division = (a, b ) => {
-//     return new Promise ((resole, reject) => {
-
-//         if(b !== 0){
-//             resole(a +' / ' + b + ' = ' + a / b)
-//         }
-
-//         reject('b doit different a zero')
-//     })
-// }
-// division(20, 2)
-// .then(res => {
-//     console.log(res)
-// })
-// .catch(err => {
-//     console.log(err);
-// })
-
-// MULTIPLICATION
-//let a = Number(prompt('enter a'))
- // let b = Number(prompt('enter b'))
-// const multiplication = (a , b ) => {
-//     return new Promise ((valid, reject) => {
-//         let egal = a * b
-//         if (b == 0 ){
+ // ADDITION
+let a = Number(prompt('enter a'))
+let b = Number(prompt('enter b'))
+  const addition = (a , b ) => {
+     return new Promise ((valid, reject) => {
+         let somme = a + b
+         if (somme <= 1 ){
 
             
             
-//             return reject('b doit difference a zero')
+             return reject('somme doit depasser a 1')
 
-//         }
+         }
 
-//         valid(`voici le resultat : ${egal}`)
-//     })
-// }
-// multiplication(a, b)
-// .then(res => {
-//     console.log(res);
-// })
-// .catch(err => {
-//     console.log(err);
-// })
-
-// soustraction
-//let a = Number(prompt('enter a'))
-//let b = Number(prompt('enter b'))
-//  const soustraction = (a , b ) => {
-//      return new Promise ((valid, reject) => {
-//          let differ = a - b
-//          if (a < b  || a <= 0){
-
-            
-            
-//              return reject('a doit supperieur a b')
-
-//          }
-
-//          valid(`voici le resultat : ${differ}`)
-//      })
-//  }
-//  soustraction(7, 2)
-//  .then(res => {
-//      console.log(res);
-//  })
-//  .catch(err => {
-//      console.log(err);
-//  })
+         valid(`voici le resultat : ${somme}`)
+     })
+ }
+ addition(a, b).then(res => {
+     console.log(res);
+ })
+ .catch(err => {
+     console.log(err);
+ })
 
 
- // Addition
-//let a = Number(prompt('enter a'))
-//let b = Number(prompt('enter b'))
- // const addition = (a , b ) => {
-//     return new Promise ((valid, reject) => {
-//         let somme = a + b
-//         if (somme <= 1 ){
+
+// MODULO
+let a = Number(prompt('enter a'))
+let b = Number(prompt('enter b'))
+  const modulo = (a , b ) => {
+     return new Promise ((valid, reject) => {
+         let somme = a % b
+         if (somme <= 1 ){
 
             
             
-//             return reject('somme doit depasser a 1')
+             return reject('somme doit depasser a 1')
 
-//         }
+         }
 
-//         valid(`voici le resultat : ${somme}`)
-//     })
-// }
-// addition(3, 2)
-// .then(res => {
-//     console.log(res);
-// })
-// .catch(err => {
-//     console.log(err);
-// })
-
+         valid(`voici le resultat : ${somme}`)
+     })
+ }
+ addition(a, b).then(res => {
+     console.log(res);
+ })
+ .catch(err => {
+     console.log(err);
+ })
 
 
 
@@ -179,4 +146,3 @@ Division de a/b avec b!=0
 
 
 
-// a + ' * ' + b + ' = ' + a * b
